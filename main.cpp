c9e01e676bfeeb467a362be741e9bb3e7ab5f0f7
almalexia1997 <tea9000@yandex.ru>
2016-03-28 21:40:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void StraightCourse(Matrix A,Matrix S,Matrix D,int size)
+/*void StraightCourse(Matrix A,Matrix S,Matrix D,int size)
 {
     double sum1=0;
     double sum2=0;
@@ -20,24 +20,56 @@ void StraightCourse(Matrix A,Matrix S,Matrix D,int size)
 
             if(i==j)
             {
-                for(int k=0;k<=i-1;++k)
+                for(int k=0;k<i;++k)
                     double sum1=sum1+D[k][k]*S[k][i]*S[k][i];
                 ((A[i][i]-sum1)>0)? D[i][i]=1 : D[i][i]=-1;
                 S[i][i]=sqrt(abs(A[i][i]-sum1));
             }
             else if(i<j)
             {
-                for(int k=0;k<=i-1;++k)
+                for(int k=0;k<i;++k)
                     sum2=sum2+D[k][k]*S[k][i]*S[k][j];
                 S[i][j]=(A[i][j]-sum2)/(S[i][i]*D[i][i]);
             }
-            else if(i>j)
+            else
+                S[i][j]=0;
+        }
+    }
+}*/
+
+void StraightCourse(Matrix A,Matrix S,Matrix D,int size)
+{
+    double sum1=0;
+    double sum2=0;
+    S[0][0]=sqrt(abs(A[0][0]));
+    for(int j=1;j<size;++j)
+        S[0][j]=A[0][j]/S[0][0];
+    for(int i=0;i<size;++i)
+    {
+        for(int j=0;j<size;++j)
+        {
+
+            if(i==j)
+            {
+                for(int k=0;k<i;++k)
+                    double sum1=sum1+S[k][i]*S[k][i];
+                S[i][i]=sqrt(abs(A[i][i]-sum1));
+            }
+            else if(i<j)
+            {
+                for(int k=0;k<i;++k)
+                    sum2=sum2+S[k][i]*S[k][j];
+                S[i][j]=(A[i][j]-sum2)/(S[i][i]);
+            }
+            else
                 S[i][j]=0;
         }
     }
 }
 
-void Reverse(Vector b, Vector x, Matrix S, Matrix D, int size)
+//
+
+/*void Reverse(Vector b, Vector x, Matrix S, Matrix D, int size)
 {
     int sum1=0;
     int sum2=0;
@@ -52,7 +84,28 @@ void Reverse(Vector b, Vector x, Matrix S, Matrix D, int size)
     x[size-1]=y[size-1]/S[size-1][size-1];
     for(int i=size;i>=0;--i)
     {
-        for(int k=size;k>=i+1;--k)
+        for(int k=size;k>i;--k)
+            sum2=sum2+S[i][k]*x[k];
+        x[i]=(y[i]-sum2)/S[i][i];
+    }
+}*/
+
+void Reverse(Vector b, Vector x, Matrix S, Matrix D, int size)
+{
+    int sum1=0;
+    int sum2=0;
+    Vector y(size);
+    y[0]=b[0]/(S[0][0]);
+    for(int i=1;i<size;++i)
+    {
+        for(int k=0;k<i;++k)
+            sum1=sum1+S[k][i]*y[k];
+        y[i]=(b[i]-sum1)/(S[i][i]);
+    }
+    x[size-1]=y[size-1]/S[size-1][size-1];
+    for(int i=size-2;i>=0;--i)
+    {
+        for(int k=size-1;k>i;--k)
             sum2=sum2+S[i][k]*x[k];
         x[i]=(y[i]-sum2)/S[i][i];
     }
@@ -62,41 +115,38 @@ int main()
 {
     int size;
     cout<<"size=";cin>>size;
-    Matrix A(size); //создаем матрицу
+    Matrix A(size); //matrix coefficients
     for(int i=0; i<size; ++i)
         for(int j=0; j<size; ++j)
         {
             cout<<"A["<<i+1<<"]["<<j+1<<"]=";
             cin>>A[i][j];
         }
-    Vector b(size); //создаем вектор свободных членов
+    Vector b(size); //We create a vector of free terms
     for (int i=0;i<size;++i)
     {
         cout<<"b["<<i+1<<"]=";
         cin>>b[i];
     }
 
-    Matrix S(size); //создаем матрицу для разложения Холецкого
-    Matrix St(size);
-    Matrix D(size);
-    StraightCourse(A,S,D,size); //прямой ход
-    St.Inverse(size,S);
-
-    Vector x(size); //искомый вектор
-    Reverse(b,x,S,D,size); //обратный ход
-
-    /*A.showData();
-    cout<<endl;
-    Matrix At(size);
-    At.Inverse(size,A);
-    At.showData();
-    cout<<endl;
+    Matrix S(size); //matrix decomposition
+    Matrix St(size);//transposed matrix decomposition
+    Matrix D(size); //diagonal matrix
+    StraightCourse(A,S,D,size); //create matrix decomposition
+    St.Inverse(size,S); //create transposed matrix decomposition
+    cout<<"\nMatrix A"<<endl;
+    A.showData();
+    cout<<"\nVector b"<<endl;
+    b.ShowData();
+    cout<<"\nMatrix S"<<endl;
     S.showData();
-    cout<<endl;
+    cout<<"\nMatrix St"<<endl;
     St.showData();
-    cout<<endl;
-    x.ShowData();*/
-    return 0;
+
+    Vector x(size); //unknown vector
+    Reverse(b,x,S,D,size);
+    cout<<"\nVector x"<<endl;
+    x.ShowData();
 };
 
 
