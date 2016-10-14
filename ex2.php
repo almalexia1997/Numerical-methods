fa7c22b97e040b56d6267572d0534b77a3fc0812
Alastor1997 <tea9000@yandex.ru>
2016-10-14 18:08:07 +0400

    Create ex2.php

---
+++ ex2.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$link = mysql_connect("localhost", "student","student") or die('Не удалось соединиться: ' . mysql_error());
+mysql_select_db("student");
+
+$result = mysql_query("SELECT * FROM TableRus") or die('Запрос не удался: ' . mysql_error());;
+
+$rows = 2; // количество строк, tr
+$cols = 7; // количество столбцов, td
+
+//Выводим результаты в html
+echo "<table> \n";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Освобождаем память от результата
+mysql_free_result($result);
+
+// Закрываем соединение
+mysql_close($link);
+?>
+
