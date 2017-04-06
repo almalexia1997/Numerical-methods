4544012e7315bd95542e2be48eaeb83be0b3b6ca
Alastor1997 <tea9000@yandex.ru>
2017-04-06 17:28:05 +0400

    Create The simplest programs II (#2).cpp

---
+++ Computer science/The simplest programs II (#2).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+usingnamespace std;
+int main()
+{
+double P,S,a;
+cout<<"Enter P"<< endl;cin>> P;
+	a = P /3;
+	S =(a*a*sqrt(3))/4;
+cout<<"S="<< S << endl;
+return0;
+}
