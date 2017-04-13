91016b643e5a62abb07199937ecf8d4f5b2f604d
Alastor1997 <tea9000@yandex.ru>
2017-04-13 14:39:42 +0400

    Update 6S ex4.cpp

--- numerical-method/6S ex4.cpp
+++ numerical-method/6S ex4.cpp
@@ -5,22 +5,28 @@
 #include <algorithm>
 using namespace std;
 
-const int N = 5, n0 = 7, l=4, T=2, c=3; //const double pi=3.14159;
+const int N = 7, n0 = 9; //const double pi=3.14159;
+const double l=4, T=2, c=1;
 
 //ПРОТОТИПЫ
-double norma(double **nor, int N, int n0); //функция нормы 
-void progonka(int n, double g0, double gN, double A, double B, double C, double *F, double *y1); // функция прогонки 
+double norma(double **nor, int N, int n0); //функция нормы
+void progonka(int n, double g0, double gN, double A, double B, double C, double *F, double *y1); // функция прогонки
 //double u(double x, double t); // точное решение
 double f(double x, double t); //правая часть волнового уравнения
 double u0(double x); // начальное условие 0-го временного слоя
-double uu0(double x, double tau); //начальное условие 1-ой производной 
+double uu0(double x, double tau); //начальное условие 1-ой производной
 double uk0(double t); // краевое условие при x=0
 double ukN(double t); // краевое условие при x=l
 
 int main()
 {
 	//1) Задаем начальные данные
-	double g0, eps, gN, A, B, C, h = l / N, tau = T / n0, sigma = 0.5, gam = tau / h;
+	double g0, eps, gN, A, B, C;
+	double h = l / N;
+	double tau = T / n0;
+	double sigma = 0.5;
+	double gam = tau / h;
+	cout<<"h="<<h<<"; gam="<<gam<<"; tau="<<tau<<"; sigma="<<sigma<<endl;
 	double *F = new double[N + 1], *y1 = new double[N + 1]; //массивы для F и y1
 	double **y = new double*[N + 1]; for (int i = 0; i < N + 1; ++i)y[i] = new double[n0 + 1];//массив для y
 	double **pogr = new double*[50]; for (int i = 0; i < 50; ++i) pogr[i] = new double[50]; //массив для pogr
@@ -30,7 +36,7 @@ int main()
 	//2) Аппроксимируем заданные функции сеточными функциями
 	for (int i = 0; i <= N; i++){
 		y[i][0] = u0(i*h); // начальные условия прибл.реш
-		y[i][1] = u0(i*h) + tau*uu0(i*h, tau); 
+		y[i][1] = u0(i*h) + tau*uu0(i*h, tau);
 	}
 	for (int k = 0; k <= n0; k++){
 		y[0][k] = uk0(k*tau); // краев. услов. прибл.решения
@@ -46,26 +52,36 @@ int main()
 			((y[i - 1][k - 1] -	2.*y[i][k - 1] + y[i + 1][k - 1]) / (h*h)) + tau*tau*f(i*h, k*tau);
 			// краевые. усл. для прогонки
 			g0 = y[0][k + 1];
-			gN = y[N][k + 1]; 
+			gN = y[N][k + 1];
 		}
 		//метод прогонки
 		progonka(N, g0, gN, A, B, C, F, y1);
 		for (int i = 0; i <= N; i++)
 			y[i][k + 1] = y1[i]; // построение решения
 	}
+	//Вывод всех результатов
 	cout << "reshenie" << endl;
 	for (int k = 0; k <= n0; k++, cout << endl)
 		for (int i = 0; i <= N; i++)
 			cout << fixed << setprecision(4) << y[i][k] << " ";
+	/*cout << "tochnoe reshenie" << endl;
+	for (int k = 0; k <= n0; k++, cout << endl)
+		for (int i = 0; i <= N; i++)
+			cout << fixed << setprecision(4) << u(i*h, k*tau) << " "; // tochnoe reshenie*/
 	cout << endl << "sigma= " << sigma << endl << endl;
 	/*for (int i = 0; i <= N; i++)
 		for (int j = 0; j <= n0; j++)
 			pogr[i][j] = fabs(y[i][j] - u(i*h, j*tau)); // погрешность
 	cout << endl << " norma pogreshnosti " << norma(pogr, N, n0) << endl;*/
-	cout << endl << " epsilon= " << eps << endl;
+	cout << endl << "epsilon= " << eps << endl;
 	return 0;
 }
 // ФУНКЦИИ
+
+/*double u(double x, double t){
+	return n*x*x*x*t*t*t + x*t + x*n + 1;
+}*/
+
 double f(double x, double t) {
 	return x*x*x*(l-x*x)*t;
 }
