bf5be4aa7da630ab722dc5a51328d4c0af74b0d6
Alastor1997 <tea9000@yandex.ru>
2018-02-10 16:39:32 +0400

    Update test.php

--- web-design/other/test.php
+++ web-design/other/test.php
@@ -1,35 +1,27 @@
 <?php
 if (isset($_GET['a']) && isset($_GET['b']) && isset($_GET['op'])){
-$a = $_GET['a'];
-$b = $_GET['b'];
-$op = $_GET['op'];
-switch ($op){
-case 'add':
-$c = $a+$b;
-print "$a+$b=$c";
-break;
-case 'sub':
-$c = $a-$b;
-print "$a-$b=$c";
-break;
-case 'mul':
-$c = $a*$b;
-print "$a*$b=$c";
-break;
-case 'div':
-if($b==0)
-{
-print 'unbelieveble';
-}
-else
-{
-$c=$a/$b;
-print "$a/$b=$c";
-}
-break;
-default: print 'no';
-}
-}else{
-print "No params!";
-}
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    $op = $_GET['op'];
+    switch ($op){
+        case 'add':
+            $c = $a+$b;
+            print "$a+$b=$c";
+            break;
+        case 'sub':
+            $c = $a-$b;
+            print "$a-$b=$c";
+            break;
+        case 'mul':
+            $c = $a*$b;
+            print "$a*$b=$c";
+            break;
+        case 'div':
+            if($b==0) print 'unbelieveble';
+            else{
+                $c=$a/$b;
+                print "$a/$b=$c";}
+            break;
+        default: print 'no';}}
+else print "No params!";
 ?>
